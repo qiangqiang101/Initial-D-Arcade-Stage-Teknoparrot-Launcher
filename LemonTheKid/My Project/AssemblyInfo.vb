d0d23817e6596aff0ba61b446533154d027e7332
qiangqiang101 <qiangqiang101@hotmail.com>
2017-11-25 11:53:00 +0800

    1.8A

--- LemonTheKid/My Project/AssemblyInfo.vb
+++ LemonTheKid/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("d6b95da5-021b-45bc-9e71-2aeb93626611")> 
+<Assembly: Guid("d6b95da5-021b-45bc-9e71-2aeb93626611")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.1.0.0")>
+<Assembly: AssemblyFileVersion("1.1.0.0")>
